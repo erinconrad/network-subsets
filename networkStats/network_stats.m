1d6cf724c6046041267231d6c2b6f73a96badde4
Erin <erincconrad@gmail.com>
2019-02-02 12:40:05 -0500

    na

--- networkStats/network_stats.m
+++ networkStats/network_stats.m
@@ -40,6 +40,7 @@ all_c_c = resampleNetwork(A,n_perm,e_f);
 %% Initialize SMC and rho arrays
 SMC = zeros(n_f,n_perm);
 rho = zeros(n_f,n_perm);
+true_cc_most_sync = zeros(n_f,n_perm);
 
 %% Loop over each fraction and get various stats
 for f = 1:n_f
@@ -69,10 +70,14 @@ for f = 1:n_f
         % Get simple matching coefficient
         SMC_temp = simple_matching_coefficient(c_c_bin,c_c_f_p_bin);
         
+        % Get the identity of the most synchronizing node (the one we would
+        % tell the surgeons to resect)
+        [~,ch_most_sync] = min(c_c_f_p);
+        
         % Fill up SMC and rho arrays
         SMC(f,i_p) = SMC_temp;
         rho(f,i_p) = rho_temp;
-        
+        true_cc_most_sync(f,i_p) = c_c(ch_most_sync);
     end
     
 end
@@ -84,22 +89,37 @@ SMC_std = nanstd(SMC,0,2);
 rho_mean = nanmean(rho,2);
 rho_std = nanstd(rho,0,2);
 
+%% How often would we resect the wrong piece of brain?
+% For each f, get the % of times most synchronizing node is actually
+% desynchronizing in the original network (how often would we tell the
+% surgeon to resect a piece of brain that is thought to be desynchronizing
+% in the original network)
+
+resect_wrong = sum((true_cc_most_sync > 0),2)/n_perm;
+
 %% Plot rho and SMC mean and std for each fraction
 figure
-set(gcf,'Position',[440 389 1001 409]);
-subplot(1,2,1)
+set(gcf,'Position',[50 389 1400 409]);
+subplot(1,3,1)
 errorbar(e_f,rho_mean,rho_std,'linewidth',2);
 xlabel('Fraction of original network included');
 ylabel('Spearman rank coefficient');
 title({'Spearman rank coefficient between original CC',...
     'and updated CC as a function of fraction of original network included'});
 
-subplot(1,2,2)
+subplot(1,3,2)
 errorbar(e_f,SMC_mean,SMC_std,'linewidth',2);
 xlabel('Fraction of original network included');
 ylabel('Simple matching coefficient');
 title({'Simple matching coefficient between original CC',...
     'and updated CC as a function of fraction of original network included'});
+
+subplot(1,3,3)
+plot(e_f,resect_wrong*100,'linewidth',2);
+xlabel('Fraction of original network included');
+ylabel('% of permutations');
+title({'Percent of time a desynchronizing node is',...
+    'labeled as the most synchronizing'});
 
 toc
 
