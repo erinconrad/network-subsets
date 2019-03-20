ce53f9a88afadee7deecc6e19cfd8fdad2ccc53f
Erin <erincconrad@gmail.com>
2019-03-20 14:13:20 -0400

    na

--- analysis/compare_cc.m
+++ analysis/compare_cc.m
@@ -1,6 +1,10 @@
-function compare_cc(true_cc_all,whichPt)
+function compare_cc(whichPts)
 
 which_sec = -5;
+
+if isempty(whichPts) == 1
+    whichPts = [1:5,8:33];
+end
 
 %% Load Stuff
 [electrodeFolder,jsonfile,scriptFolder,resultsFolder,...
@@ -12,20 +16,39 @@ addpath(p1);
 addpath([bctFolder]);
 
 load([dataFolder,'structs/info.mat']);
+true_cc_folder_base = [dataFolder,'control_centralities/'];
 
-[adj,~] = reconcileAdj(pt,whichPt);
-A_all = adj(4).data;
-A = squeeze(A_all(ceil(size(A_all,1)/2)+which_sec,:,:));
+for whichPt = whichPts
 
-true_cc = true_cc_all(ceil(size(true_cc_all,1)/2)+which_sec,:)';
-%A = squeeze(A_all(1,:,:));
-%true_cc = true_cc_all(1,:)';
+    % Load new true cc    
+    name = pt(whichPt).name;
+    true_cc_folder = [true_cc_folder_base,name,'/'];
+    load([true_cc_folder,'cc_true.mat']);
+    true_cc_all = cc;
 
-cc = control_centrality(A);
+    [adj,~] = reconcileAdj(pt,whichPt);
+    A_all = adj(4).data;
+    A = squeeze(A_all(ceil(size(A_all,1)/2)+which_sec,:,:));
 
-locs = pt(whichPt).new_elecs.locs; 
+    true_cc = true_cc_all(ceil(size(true_cc_all,1)/2)+which_sec,:)';
+    %A = squeeze(A_all(1,:,:));
+    %true_cc = true_cc_all(1,:)';
+
+    cc_new = control_centrality(A);
+
+    locs = pt(whichPt).new_elecs.locs; 
+    
+    max_dev = max(abs(cc_new-true_cc));
+    if max_dev > 0.0001
+        fprintf('WARNING, discrepancy for %s\n\n\n\n',name);
+    else
+        fprintf('Max dev for %s is %1.2e\n\n',name,max_dev);
+    end
 
 
+end
+
+%{
 figure
 subplot(1,2,1)
 scatter3(locs(:,1),locs(:,2),locs(:,3),100,'k');
@@ -38,6 +61,7 @@ scatter3(locs(:,1),locs(:,2),locs(:,3),100,'k');
 scatter3(locs(:,1),locs(:,2),locs(:,3),100,cc./true_cc,'filled');
 set(gca,'clim',prctile(cc./true_cc,[10 90]));
 colorbar
+%}
 
 
 end
