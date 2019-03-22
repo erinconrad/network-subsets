b1341bf2ccf80995c48db3bc9f32c3a12512e2c0
Erin <erincconrad@gmail.com>
2019-03-22 10:26:05 -0400

    added reliability measure

--- networkStats/reliability_nodal.m
+++ networkStats/reliability_nodal.m
@@ -1,4 +1,4 @@
-function reliability_nodal(perm_metric,true_metric)
+function reliability = reliability_nodal(perm_metric,true_metric)
 
 %{
 Total variance = variance of true scores + variance of error
@@ -9,7 +9,7 @@ variance_error = nanmean(squeeze(nanstd(perm_metric,0,3).^2),1);
 
 variance_true = nanstd(true_metric,0,1).^2;
 
-reliability = variance_true/(variance_true + variance_total);
+reliability = variance_true/(variance_true + variance_error);
 
 
 end
